f1416ff032dc996534bca97b1a30696a5de6444c
phelrine <phelrine@gmail.com>
2011-09-11 01:51:58 +0900

    check

--- public/js/typing.coffee
+++ public/js/typing.coffee
@@ -1,20 +1,52 @@
+rg = null
+
+$(document).ready ->
+  text = "てすとぶんしょう"
+  $("#text").text text
+  rg = new RomanizationGraph text
+  $("#romaji").text rg.decode()
+
+$(document).keydown (event)->
+  $("#input").text("")
+  chr = String.fromCharCode(event.keyCode).toLowerCase()
+  if rg.check chr
+    $("#inputed").text($("#inputed").text() + chr)
+  else
+    $("#input").text(chr)
+
+clone = (obj) ->
+  clone = ->
+  clone.prototype = obj
+  return new clone;
+
 class RomanizationGraph
   constructor: (sentence)->
+    @graph = []
     @pos = 0
-    @fixed = []
     index = 0
-    @graph = for chr in sentence
+    nflag = false
+    for chr in sentence
       node = []
       nextIndex = index + 1
       nextChr = sentence[nextIndex]
-      if chr is "っ" and nextChr? and nextChr isnt "ん"
-        codes = Dict.conv(nextChr + sentence[nextIndex + 1]) if sentence[nextIndex + 1]?
+
+      if chr is "っ" and sentence[nextIndex]? and nextChr isnt "ん"
+        codes = if sentence[nextIndex + 1]? then Dict.conv(nextChr + sentence[nextIndex + 1]) else []
         node = node.concat((new Code (code[0] + code).split(''), nextIndex + 2) for code in codes)
         node = node.concat((new Code (code[0] + code).split(''), nextIndex + 1) for code in Dict.conv(nextChr))
       node.push new Code code.split(''), (nextIndex + 1) for code in Dict.conv(chr + nextChr) if nextChr?
       node.push new Code code.split(''), (nextIndex) for code in Dict.conv(chr)
+
+      if nflag
+        nnode = for code in node
+          ncode = new Code code.code.concat(), code.next
+          ncode.code.unshift "n"
+          ncode
+        node = node.concat(nnode)
+
+      @graph.push node
+      nflag = (chr is "ん")
       index++
-      node
 
   decode: ->
     node = @graph[@pos]
@@ -24,6 +56,21 @@ class RomanizationGraph
       seq
     ret.join('')
 
+  check: (chr)->
+    return false unless @pos < @graph.length
+    pos = @pos
+    flag = false
+    for code in @graph[@pos]
+      if code.code[0] is chr
+        console.log code.code
+        console.log code.next
+        code.code.shift()
+        if code.code.length is 0
+          pos = code.next
+          console.log @graph[pos]
+        flag = true
+    @pos = pos
+    flag
 
 class Dict
   @conv: (chr)->
--- public/js/typing.js
+++ public/js/typing.js
@@ -1,59 +1,91 @@
-var Code, Dict, RomanizationGraph;
+var Code, Dict, RomanizationGraph, clone, rg;
+rg = null;
+$(document).ready(function() {
+  var text;
+  text = "てすとぶんしょう";
+  $("#text").text(text);
+  rg = new RomanizationGraph(text);
+  return $("#romaji").text(rg.decode());
+});
+$(document).keydown(function(event) {
+  var chr;
+  $("#input").text("");
+  chr = String.fromCharCode(event.keyCode).toLowerCase();
+  if (rg.check(chr)) {
+    return $("#inputed").text($("#inputed").text() + chr);
+  } else {
+    return $("#input").text(chr);
+  }
+});
+clone = function(obj) {
+  clone = function() {};
+  clone.prototype = obj;
+  return new clone;
+};
 RomanizationGraph = (function() {
   function RomanizationGraph(sentence) {
-    var chr, code, codes, index, nextChr, nextIndex, node;
+    var chr, code, codes, index, ncode, nextChr, nextIndex, nflag, nnode, node, _i, _j, _k, _len, _len2, _len3, _ref, _ref2;
+    this.graph = [];
     this.pos = 0;
-    this.fixed = [];
     index = 0;
-    this.graph = (function() {
-      var _i, _j, _k, _len, _len2, _len3, _ref, _ref2, _results;
-      _results = [];
-      for (_i = 0, _len = sentence.length; _i < _len; _i++) {
-        chr = sentence[_i];
-        node = [];
-        nextIndex = index + 1;
-        nextChr = sentence[nextIndex];
-        if (chr === "っ" && (nextChr != null) && nextChr !== "ん") {
-          if (sentence[nextIndex + 1] != null) {
-            codes = Dict.conv(nextChr + sentence[nextIndex + 1]);
+    nflag = false;
+    for (_i = 0, _len = sentence.length; _i < _len; _i++) {
+      chr = sentence[_i];
+      node = [];
+      nextIndex = index + 1;
+      nextChr = sentence[nextIndex];
+      if (chr === "っ" && (sentence[nextIndex] != null) && nextChr !== "ん") {
+        codes = sentence[nextIndex + 1] != null ? Dict.conv(nextChr + sentence[nextIndex + 1]) : [];
+        node = node.concat((function() {
+          var _j, _len2, _results;
+          _results = [];
+          for (_j = 0, _len2 = codes.length; _j < _len2; _j++) {
+            code = codes[_j];
+            _results.push(new Code((code[0] + code).split(''), nextIndex + 2));
           }
-          node = node.concat((function() {
-            var _j, _len2, _results2;
-            _results2 = [];
-            for (_j = 0, _len2 = codes.length; _j < _len2; _j++) {
-              code = codes[_j];
-              _results2.push(new Code((code[0] + code).split(''), nextIndex + 2));
-            }
-            return _results2;
-          })());
-          node = node.concat((function() {
-            var _j, _len2, _ref, _results2;
-            _ref = Dict.conv(nextChr);
-            _results2 = [];
-            for (_j = 0, _len2 = _ref.length; _j < _len2; _j++) {
-              code = _ref[_j];
-              _results2.push(new Code((code[0] + code).split(''), nextIndex + 1));
-            }
-            return _results2;
-          })());
-        }
-        if (nextChr != null) {
-          _ref = Dict.conv(chr + nextChr);
+          return _results;
+        })());
+        node = node.concat((function() {
+          var _j, _len2, _ref, _results;
+          _ref = Dict.conv(nextChr);
+          _results = [];
           for (_j = 0, _len2 = _ref.length; _j < _len2; _j++) {
             code = _ref[_j];
-            node.push(new Code(code.split(''), nextIndex + 1));
+            _results.push(new Code((code[0] + code).split(''), nextIndex + 1));
           }
-        }
-        _ref2 = Dict.conv(chr);
-        for (_k = 0, _len3 = _ref2.length; _k < _len3; _k++) {
-          code = _ref2[_k];
-          node.push(new Code(code.split(''), nextIndex));
-        }
-        index++;
-        _results.push(node);
+          return _results;
+        })());
       }
-      return _results;
-    })();
+      if (nextChr != null) {
+        _ref = Dict.conv(chr + nextChr);
+        for (_j = 0, _len2 = _ref.length; _j < _len2; _j++) {
+          code = _ref[_j];
+          node.push(new Code(code.split(''), nextIndex + 1));
+        }
+      }
+      _ref2 = Dict.conv(chr);
+      for (_k = 0, _len3 = _ref2.length; _k < _len3; _k++) {
+        code = _ref2[_k];
+        node.push(new Code(code.split(''), nextIndex));
+      }
+      if (nflag) {
+        nnode = (function() {
+          var _l, _len4, _results;
+          _results = [];
+          for (_l = 0, _len4 = node.length; _l < _len4; _l++) {
+            code = node[_l];
+            ncode = new Code(code.code.concat(), code.next);
+            ncode.code.unshift("n");
+            _results.push(ncode);
+          }
+          return _results;
+        })();
+        node = node.concat(nnode);
+      }
+      this.graph.push(node);
+      nflag = chr === "ん";
+      index++;
+    }
   }
   RomanizationGraph.prototype.decode = function() {
     var node, ret, seq;
@@ -69,6 +101,30 @@ RomanizationGraph = (function() {
       return _results;
     }).call(this);
     return ret.join('');
+  };
+  RomanizationGraph.prototype.check = function(chr) {
+    var code, flag, pos, _i, _len, _ref;
+    if (!(this.pos < this.graph.length)) {
+      return false;
+    }
+    pos = this.pos;
+    flag = false;
+    _ref = this.graph[this.pos];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      code = _ref[_i];
+      if (code.code[0] === chr) {
+        console.log(code.code);
+        console.log(code.next);
+        code.code.shift();
+        if (code.code.length === 0) {
+          pos = code.next;
+          console.log(this.graph[pos]);
+        }
+        flag = true;
+      }
+    }
+    this.pos = pos;
+    return flag;
   };
   return RomanizationGraph;
 })();
