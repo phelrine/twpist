d942ef30a1ea8d4b2c98eac3aae2607a88dacebc
phelrine <phelrine@gmail.com>
2011-09-10 18:14:43 +0900

    twitter認証

--- app.rb
+++ app.rb
@@ -1,15 +1,55 @@
+require 'bundler/setup'
 require 'sinatra/base'
+require 'omniauth'
+require 'rubytter'
 require 'json'
 require 'nkf'
 require 'MeCab'
 
-class TwpistApp < Sinatra::Base
-  get '/' do
-    "twpist"
-  end
 
-  get '/yomi.json' do
+class TwpistApp < Sinatra::Base
+  enable :sessions, :logging
+  
+  configure do
+    CONSUMER_KEY, CONSUMER_SECRET = File.open(".consumer").read.split
+  end
+  
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :twitter, CONSUMER_KEY, CONSUMER_SECRET
+  end
+  
+  helpers do
+    def consumer 
+      @consumer ||= OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: "https://api.twitter.com/")
+    end
+  end
+  
+  get '/' do
+    "twpist<br><a href='/auth/twitter'>twitter</a><br>"
+  end
+  
+  get 'game' do
+    
+  end
+  
+  get '/timeline.json' do
+    if session[:user]
+      rubytter = OAuthRubytter.new(OAuth::AccessToken.new consumer, session[:token], session[:secret])
+    end
     content_type :json
-    return {yomi: NKF.nkf("-w --hiragana", MeCab::Tagger.new("-O yomi").parse(params[:text])).chomp}.to_json
+    rubytter.friends_timeline(:count => 200).to_a.map{|status|
+      status[:yomi] = NKF.nkf("-w --hiragana", MeCab::Tagger.new("-O yomi").parse(status.text)).chomp
+      status
+    }.to_json
+  end
+  
+  get '/auth/twitter/callback' do
+    auth = request.env["omniauth.auth"]
+    puts auth
+    session[:user] = auth["uid"]
+    session[:token] = auth["credentials"]["token"]
+    session[:secret] = auth["credentials"]["secret"]
+    redirect '/game'
   end
 end
