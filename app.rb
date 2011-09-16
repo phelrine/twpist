24bf158ef0bb24c497347d0f47d0e976536a8a5c
phelrine <phelrine@gmail.com>
2011-09-16 16:20:28 +0900

    1.8

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class TwpistApp < Sinatra::Base
   
   helpers do
     def consumer 
-      @consumer ||= OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: "https://api.twitter.com/")
+      @consumer ||= OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, :site => "https://api.twitter.com/")
     end
   end
   
