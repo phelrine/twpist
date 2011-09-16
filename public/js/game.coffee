fb46ccefdb7bebfd0288c9bc75d528783480b808
phelrine <phelrine@gmail.com>
2011-09-16 23:14:26 +0900

    result追加

--- public/js/game.coffee
+++ public/js/game.coffee
@@ -8,6 +8,7 @@ class Twpist
   loadAssignment: (regex)->
     @index = -1
     @count = 0
+    @allType = 0
     $("div.level-container").hide "slow"
 
     $.get "/timeline.json", (timeline)=>
@@ -20,12 +21,14 @@ class Twpist
       $(document).keydown (event)=>
         chr = String.fromCharCode(event.keyCode).toLowerCase()
         if @traverser.traverse chr
+          @allType++
           fixed = $("div.typing-inputarea h2.fixed")
           fixed.text @traverser.getFixedText()
           fixed.scrollLeft fixed.get(0).scrollWidth
           $("div.typing-inputarea h2.input").text @traverser.decode()
           if @traverser.hasFinished()
-            tweet = $(new EJS(url: "ejs/tweet.ejs").render(status: @timeline[@index]))
+            time = new Date() - @startTime
+            tweet = $(new EJS(url: "ejs/tweet.ejs").render(status: @timeline[@index], time: time))
             $("ul.timeline").prepend tweet.hide()
             tweet.show "slow"
             @nextAssignment()
@@ -36,11 +39,10 @@ class Twpist
 
   nextAssignment: ->
     @index++
-    console.log @index
     @index++ while @timeline[@index].yomi.length is 0
     status = @timeline[@index]
     @traverser = new RomanizationGraph(status.yomi).traverser()
-
+    @startTime = new Date
     $("div.typing-inputarea h2.fixed").text ""
     $("div.typing-container img.icon").attr src: status.user.profile_image_url
     $("h2.typing-assignment").text status.text
@@ -52,11 +54,31 @@ class Twpist
     @showResult() if @count is 140
 
   showResult: ->
-    console.log "show"
     clearInterval(@timer)
     $("div.controller-container").hide()
     $("ul.timeline").hide()
     $(".result").show()
     $("ul.tabs li.timeline").removeClass "active"
     $("ul.tabs li.result").addClass "active"
-    $("div.typing-container div.result").append new EJS(url: "ejs/result_tweet.ejs").render()
+    resultEjs = new EJS(url: "ejs/result_tweet.ejs")
+    $("div.typing-container div.result").append resultEjs.render()
+    tweetEjs = new EJS(url: "ejs/tweet.ejs")
+    $("ul.result").append tweetEjs.render
+      status: new ProxyStatus @allType + "文字", "img/icon.gif", "総入力文字数"
+    $("ul.result").append tweetEjs.render
+      status: new ProxyStatus @index + "ツイート", "img/icon.gif", "総入力ツイート数"
+    $("ul.result").append tweetEjs.render
+      status: new ProxyStatus (25600 / @allType).toFixed(3) + "秒", "img/icon.gif",
+        "140文字打つのにかかる時間"
+    $("ul.result").append tweetEjs.render
+      status: new ProxyStatus (140 / @index).toFixed(3) + "秒/ツイート", "img/icon.gif", "平均ツイート時間"
+    $("ul.result").append tweetEjs.render
+      status: new ProxyStatus (650 * @index).toFixed(3)  + "ツイート", "img/icon.gif", "一日でツイートできる回数"
+
+
+class ProxyStatus
+  constructor: (@text, image, screen, name = screen)->
+    @user =
+      profile_image_url: image
+      screen_name: screen
+      name: name
--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,4 @@
-var Twpist;
+var ProxyStatus, Twpist;
 var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 $(document).ready(function() {
   var twpist;
@@ -12,6 +12,7 @@ Twpist = (function() {
   Twpist.prototype.loadAssignment = function(regex) {
     this.index = -1;
     this.count = 0;
+    this.allType = 0;
     $("div.level-container").hide("slow");
     $.get("/timeline.json", __bind(function(timeline) {
       var status, _i, _len;
@@ -23,18 +24,21 @@ Twpist = (function() {
       this.nextAssignment();
       $("div.controller-container").show();
       $(document).keydown(__bind(function(event) {
-        var chr, fixed, tweet;
+        var chr, fixed, time, tweet;
         chr = String.fromCharCode(event.keyCode).toLowerCase();
         if (this.traverser.traverse(chr)) {
+          this.allType++;
           fixed = $("div.typing-inputarea h2.fixed");
           fixed.text(this.traverser.getFixedText());
           fixed.scrollLeft(fixed.get(0).scrollWidth);
           $("div.typing-inputarea h2.input").text(this.traverser.decode());
           if (this.traverser.hasFinished()) {
+            time = new Date() - this.startTime;
             tweet = $(new EJS({
               url: "ejs/tweet.ejs"
             }).render({
-              status: this.timeline[this.index]
+              status: this.timeline[this.index],
+              time: time
             }));
             $("ul.timeline").prepend(tweet.hide());
             tweet.show("slow");
@@ -52,12 +56,12 @@ Twpist = (function() {
   Twpist.prototype.nextAssignment = function() {
     var status;
     this.index++;
-    console.log(this.index);
     while (this.timeline[this.index].yomi.length === 0) {
       this.index++;
     }
     status = this.timeline[this.index];
     this.traverser = new RomanizationGraph(status.yomi).traverser();
+    this.startTime = new Date;
     $("div.typing-inputarea h2.fixed").text("");
     $("div.typing-container img.icon").attr({
       src: status.user.profile_image_url
@@ -73,16 +77,49 @@ Twpist = (function() {
     }
   };
   Twpist.prototype.showResult = function() {
-    console.log("show");
+    var resultEjs, tweetEjs;
     clearInterval(this.timer);
     $("div.controller-container").hide();
     $("ul.timeline").hide();
     $(".result").show();
     $("ul.tabs li.timeline").removeClass("active");
     $("ul.tabs li.result").addClass("active");
-    return $("div.typing-container div.result").append(new EJS({
+    resultEjs = new EJS({
       url: "ejs/result_tweet.ejs"
-    }).render());
+    });
+    $("div.typing-container div.result").append(resultEjs.render());
+    tweetEjs = new EJS({
+      url: "ejs/tweet.ejs"
+    });
+    $("ul.result").append(tweetEjs.render({
+      status: new ProxyStatus(this.allType + "文字", "img/icon.gif", "総入力文字数")
+    }));
+    $("ul.result").append(tweetEjs.render({
+      status: new ProxyStatus(this.index + "ツイート", "img/icon.gif", "総入力ツイート数")
+    }));
+    $("ul.result").append(tweetEjs.render({
+      status: new ProxyStatus((25600 / this.allType).toFixed(3) + "秒", "img/icon.gif")
+    }, "140文字打つのにかかる時間"));
+    $("ul.result").append(tweetEjs.render({
+      status: new ProxyStatus((140 / this.index).toFixed(3) + "秒/ツイート", "img/icon.gif", "平均ツイート時間")
+    }));
+    return $("ul.result").append(tweetEjs.render({
+      status: new ProxyStatus((650 * this.index).toFixed(3) + "ツイート", "img/icon.gif", "一日でツイートできる回数")
+    }));
   };
   return Twpist;
+})();
+ProxyStatus = (function() {
+  function ProxyStatus(text, image, screen, name) {
+    this.text = text;
+    if (name == null) {
+      name = screen;
+    }
+    this.user = {
+      profile_image_url: image,
+      screen_name: screen,
+      name: name
+    };
+  }
+  return ProxyStatus;
 })();
