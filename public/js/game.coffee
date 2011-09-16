a4fecb5f9f9f78d794275f34130f6d60468fc4f5
phelrine <phelrine@gmail.com>
2011-09-16 21:53:01 +0900

    クラス化

--- public/js/game.coffee
+++ public/js/game.coffee
@@ -1,63 +1,62 @@
-twpist ?= {}
-twpist.timeline = null
-twpist.index = 0
-twpist.traverser = null
-twpist.count = 0
-
-setAssignment = ->
-  $("div.typing-inputarea h2.fixed").text ""
-  status = twpist.timeline[twpist.index]
-  status = twpist.timeline[++twpist.index] while status.yomi.length is 0
-  console.log status.user.profile_image_url
-  $("div.typing-container img.icon").attr src: status.user.profile_image_url
-  $("h2.typing-assignment").text status.text
-  twpist.traverser = (new RomanizationGraph status.yomi).traverser()
-  $("div.typing-inputarea h2.input").text twpist.traverser.decode()
-
-prependTweet = (status) ->
-  tweet = $(new EJS(url: "ejs/tweet.ejs").render(status: status))
-  $("ul.timeline").prepend tweet.hide()
-  tweet.show "slow"
-
-countUp = ()->
-  twpist.count++
-  if twpist.count is 10
-    showResult()
-
-showResult = ()->
-  $("div.controller-container").hide()
-  $("ul.timeline").hide()
-  $(".result").show()
-  $("ul.tabs li.timeline").removeClass "active"
-  $("ul.tabs li.result").addClass "active"
-  $("div.typing-container div.result").append new EJS(url: "ejs/result_tweet.ejs").render()
-  $("div.timeline-container div.result").append new EJS(url: "ejs/result.ejs").render()
-loadAssignment = (regex)->
-  $("div.level-container").hide "slow"
-  $.get "/timeline.json", (timeline)->
-    status.yomi = status.yomi.replace regex, "" for status in timeline
-    twpist.timeline = timeline.reverse()
-    setAssignment()
-    $("div.controller-container").show()
-
-    $(document).keydown (event)->
-      chr = String.fromCharCode(event.keyCode).toLowerCase()
-      if twpist.traverser.traverse chr
-        fixed = $("div.typing-inputarea h2.fixed")
-        fixed.text twpist.traverser.getFixedText()
-        fixed.scrollLeft fixed.get(0).scrollWidth
-        $("div.typing-inputarea h2.input").text twpist.traverser.decode()
-        if twpist.traverser.hasFinished()
-          prependTweet twpist.timeline[twpist.index]
-          twpist.index++
-          setAssignment()
-        false
-
-    setInterval(countUp, 1000)
-  false
-
 $(document).ready ->
-  $("a.btn.easy").click -> loadAssignment /[^ぁ-ん]+/g
+  twpist = new Twpist
+  $("a.btn.easy").click -> twpist.loadAssignment /[^ぁ-ん]+/g
   # $("a.btn.normal").click -> loadAssignment /[^ぁ-ん1-9]+/g
   # $("a.btn.hard").click -> loadAssignment /[^ぁ-ん1-9a-zA-Z]+/g
-  # loadAssignment /[^ぁ-ん]+/g
+
+class Twpist
+  loadAssignment: (regex)->
+    @index = -1
+    @count = 0
+    $("div.level-container").hide "slow"
+
+    $.get "/timeline.json", (timeline)=>
+      status.yomi = status.yomi.replace regex, "" for status in timeline
+      @timeline = timeline.reverse()
+      @nextAssignment()
+
+      $("div.controller-container").show()
+
+      $(document).keydown (event)=>
+        chr = String.fromCharCode(event.keyCode).toLowerCase()
+        if @traverser.traverse chr
+          fixed = $("div.typing-inputarea h2.fixed")
+          fixed.text @traverser.getFixedText()
+          fixed.scrollLeft fixed.get(0).scrollWidth
+          $("div.typing-inputarea h2.input").text @traverser.decode()
+          if @traverser.hasFinished()
+            tweet = $(new EJS(url: "ejs/tweet.ejs").render(status: @timeline[@index]))
+            $("ul.timeline").prepend tweet.hide()
+            tweet.show "slow"
+            @nextAssignment()
+          false
+
+      @timer = setInterval((=> @countUp()), 1000)
+    false
+
+  nextAssignment: ->
+    @index++
+    console.log @index
+    @index++ while @timeline[@index].yomi.length is 0
+    status = @timeline[@index]
+    @traverser = new RomanizationGraph(status.yomi).traverser()
+
+    $("div.typing-inputarea h2.fixed").text ""
+    $("div.typing-container img.icon").attr src: status.user.profile_image_url
+    $("h2.typing-assignment").text status.text
+    $("div.typing-inputarea h2.input").text @traverser.decode()
+
+  countUp: ->
+    @count++
+    $("h2.left-time").text (140 - @count)
+    @showResult() if @count is 140
+
+  showResult: ->
+    console.log "show"
+    clearInterval(@timer)
+    $("div.controller-container").hide()
+    $("ul.timeline").hide()
+    $(".result").show()
+    $("ul.tabs li.timeline").removeClass "active"
+    $("ul.tabs li.result").addClass "active"
+    $("div.typing-container div.result").append new EJS(url: "ejs/result_tweet.ejs").render()
--- public/js/game.js
+++ public/js/game.js
@@ -1,88 +1,88 @@
-var countUp, loadAssignment, prependTweet, setAssignment, showResult;
-if (typeof twpist === "undefined" || twpist === null) {
-  twpist = {};
-}
-twpist.timeline = null;
-twpist.index = 0;
-twpist.traverser = null;
-twpist.count = 0;
-setAssignment = function() {
-  var status;
-  $("div.typing-inputarea h2.fixed").text("");
-  status = twpist.timeline[twpist.index];
-  while (status.yomi.length === 0) {
-    status = twpist.timeline[++twpist.index];
-  }
-  console.log(status.user.profile_image_url);
-  $("div.typing-container img.icon").attr({
-    src: status.user.profile_image_url
-  });
-  $("h2.typing-assignment").text(status.text);
-  twpist.traverser = (new RomanizationGraph(status.yomi)).traverser();
-  return $("div.typing-inputarea h2.input").text(twpist.traverser.decode());
-};
-prependTweet = function(status) {
-  var tweet;
-  tweet = $(new EJS({
-    url: "ejs/tweet.ejs"
-  }).render({
-    status: status
-  }));
-  $("ul.timeline").prepend(tweet.hide());
-  return tweet.show("slow");
-};
-countUp = function() {
-  twpist.count++;
-  if (twpist.count === 10) {
-    return showResult();
-  }
-};
-showResult = function() {
-  $("div.controller-container").hide();
-  $("ul.timeline").hide();
-  $(".result").show();
-  $("ul.tabs li.timeline").removeClass("active");
-  $("ul.tabs li.result").addClass("active");
-  $("div.typing-container div.result").append(new EJS({
-    url: "ejs/result_tweet.ejs"
-  }).render());
-  return $("div.timeline-container div.result").append(new EJS({
-    url: "ejs/result.ejs"
-  }).render());
-};
-loadAssignment = function(regex) {
-  $("div.level-container").hide("slow");
-  $.get("/timeline.json", function(timeline) {
-    var status, _i, _len;
-    for (_i = 0, _len = timeline.length; _i < _len; _i++) {
-      status = timeline[_i];
-      status.yomi = status.yomi.replace(regex, "");
-    }
-    twpist.timeline = timeline.reverse();
-    setAssignment();
-    $("div.controller-container").show();
-    $(document).keydown(function(event) {
-      var chr, fixed;
-      chr = String.fromCharCode(event.keyCode).toLowerCase();
-      if (twpist.traverser.traverse(chr)) {
-        fixed = $("div.typing-inputarea h2.fixed");
-        fixed.text(twpist.traverser.getFixedText());
-        fixed.scrollLeft(fixed.get(0).scrollWidth);
-        $("div.typing-inputarea h2.input").text(twpist.traverser.decode());
-        if (twpist.traverser.hasFinished()) {
-          prependTweet(twpist.timeline[twpist.index]);
-          twpist.index++;
-          setAssignment();
-        }
-        return false;
-      }
-    });
-    return setInterval(countUp, 1000);
-  });
-  return false;
-};
+var Twpist;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 $(document).ready(function() {
+  var twpist;
+  twpist = new Twpist;
   return $("a.btn.easy").click(function() {
-    return loadAssignment(/[^ぁ-ん]+/g);
+    return twpist.loadAssignment(/[^ぁ-ん]+/g);
   });
 });
+Twpist = (function() {
+  function Twpist() {}
+  Twpist.prototype.loadAssignment = function(regex) {
+    this.index = -1;
+    this.count = 0;
+    $("div.level-container").hide("slow");
+    $.get("/timeline.json", __bind(function(timeline) {
+      var status, _i, _len;
+      for (_i = 0, _len = timeline.length; _i < _len; _i++) {
+        status = timeline[_i];
+        status.yomi = status.yomi.replace(regex, "");
+      }
+      this.timeline = timeline.reverse();
+      this.nextAssignment();
+      $("div.controller-container").show();
+      $(document).keydown(__bind(function(event) {
+        var chr, fixed, tweet;
+        chr = String.fromCharCode(event.keyCode).toLowerCase();
+        if (this.traverser.traverse(chr)) {
+          fixed = $("div.typing-inputarea h2.fixed");
+          fixed.text(this.traverser.getFixedText());
+          fixed.scrollLeft(fixed.get(0).scrollWidth);
+          $("div.typing-inputarea h2.input").text(this.traverser.decode());
+          if (this.traverser.hasFinished()) {
+            tweet = $(new EJS({
+              url: "ejs/tweet.ejs"
+            }).render({
+              status: this.timeline[this.index]
+            }));
+            $("ul.timeline").prepend(tweet.hide());
+            tweet.show("slow");
+            this.nextAssignment();
+          }
+          return false;
+        }
+      }, this));
+      return this.timer = setInterval((__bind(function() {
+        return this.countUp();
+      }, this)), 1000);
+    }, this));
+    return false;
+  };
+  Twpist.prototype.nextAssignment = function() {
+    var status;
+    this.index++;
+    console.log(this.index);
+    while (this.timeline[this.index].yomi.length === 0) {
+      this.index++;
+    }
+    status = this.timeline[this.index];
+    this.traverser = new RomanizationGraph(status.yomi).traverser();
+    $("div.typing-inputarea h2.fixed").text("");
+    $("div.typing-container img.icon").attr({
+      src: status.user.profile_image_url
+    });
+    $("h2.typing-assignment").text(status.text);
+    return $("div.typing-inputarea h2.input").text(this.traverser.decode());
+  };
+  Twpist.prototype.countUp = function() {
+    this.count++;
+    $("h2.left-time").text(140 - this.count);
+    if (this.count === 140) {
+      return this.showResult();
+    }
+  };
+  Twpist.prototype.showResult = function() {
+    console.log("show");
+    clearInterval(this.timer);
+    $("div.controller-container").hide();
+    $("ul.timeline").hide();
+    $(".result").show();
+    $("ul.tabs li.timeline").removeClass("active");
+    $("ul.tabs li.result").addClass("active");
+    return $("div.typing-container div.result").append(new EJS({
+      url: "ejs/result_tweet.ejs"
+    }).render());
+  };
+  return Twpist;
+})();
