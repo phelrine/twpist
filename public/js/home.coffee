9119ab02159b77f94cdec3b7e676f7265eabbf7b
Akira Shirai <w.ackyla@gmail.com>
2011-09-30 15:05:15 +0900

    homeアニメーション追加

--- public/js/home.coffee
+++
@@ -1,3 +0,0 @@
-$(document).ready ->
-        x = Math.floor(Math.random() * 3) + 1
-        $("a#main-img-0#{x}").css("display", "block")
--- public/js/home.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  var x;
-  x = Math.floor(Math.random() * 3) + 1;
-  return $("a#main-img-0" + x).css("display", "block");
-});
